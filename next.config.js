31f65f34f947de636fd3c929ff5942bf0bcd449e
qq212qq <2122496617@qq.com>
2026-06-04 10:58:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  basePath: '/python-assistant',
-  assetPrefix: '/python-assistant'
+  trailingSlash: true
 }
 module.exports = nextConfig
